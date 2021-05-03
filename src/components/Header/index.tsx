7dd8c452ae308b11686c49b89a4c0e3e055487dc
Mauricio Freitas <mauricio.freitas@hotmail.com>
2021-05-03 16:42:27 -0300

    integração CMS Prismic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './styles.module.scss'
 import SignInButton from '../SignInButton'
+import { ActiveLink } from '../ActiveLink'
 
 export default function Header() {
     return (
@@ -8,8 +9,19 @@ export default function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    {/* o "/" manda pra home page
+
+                    sempre que a pagina é carregada, todos os componentes são carregados.
+                    com o Link, isso é corrigido, ou seja, tudo é carregado apenas 1x, depois somente os componentes essenciais, 
+                    como componentes de banco, interface com API, etc. 
+                    Assim, o carregamento de pagina fica muito mais rapido*/}
+                    <ActiveLink activeClassName={styles.active} href="/" >
+                        <a>Home</a>
+                    </ActiveLink>
+                    {/* essa funcionalidade do prefetch (<Link href="/posts" prefetch>), ja deixa tudo carregado. assim, o tempo do usuário clicar e a pagina carregar vai a zero. */}
+                    <ActiveLink activeClassName={styles.active} href="/posts">
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
